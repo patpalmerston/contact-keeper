d02a8d789dc96077b9c3eca2f4732338776ab55f
Pat Palmerston <patpalmerston@gmail.com>
2019-07-20 12:03:19 -0700

    create user and resonded with json web token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -46,7 +49,24 @@ router.post(
 
 			await user.save();
 
-			res.send('User saved');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					// use 3600 = an hour in production
+					expiresIn: 360000
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send('server error');
