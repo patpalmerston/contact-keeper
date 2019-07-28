7de7c565de9dc2ab71195b8d8671593a92b57f1e
Pat Palmerston <patpalmerston@gmail.com>
2019-07-27 20:16:40 -0700

    get logged in users contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,33 +1,45 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contact
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts')
+router.get('/', auth, async (req, res) => {
+	try {
+		const contacts = await Contact.find({ user: req.user.id }).sort({
+			date: -1
+		});
+		res.json(contacts);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('server error');
+	}
 });
 
 // @route   Post api/contacts
 // @desc    Add new contact
 // @access  Private
 router.post('/', (req, res) => {
-  res.send('Add contact')
+	res.send('Add contact');
 });
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
 router.put('/:id', (req, res) => {
-  res.send('Update contact')
+	res.send('Update contact');
 });
 
 // @route   DELETE api/contact/:id
 // @desc    Delete contact
 // @access  Private
 router.delete('/', (req, res) => {
-  res.send('Delete contact')
+	res.send('Delete contact');
 });
 
-module.exports = router
+module.exports = router;
