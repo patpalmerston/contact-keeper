d3ee5cf9fb7f0ce1832021bff84e8385168219c3
Pat Palmerston <patpalmerston@gmail.com>
2019-07-27 20:43:41 -0700

    create contacts functional

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,7 +12,7 @@ const Contact = require('../models/Contact');
 router.get('/', auth, async (req, res) => {
 	try {
 		const contacts = await Contact.find({ user: req.user.id }).sort({
-			date: -1
+			date: -1 //grabs the most recent user
 		});
 		res.json(contacts);
 	} catch (err) {
@@ -24,9 +24,43 @@ router.get('/', auth, async (req, res) => {
 // @route   Post api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-	res.send('Add contact');
-});
+// adding the auth middleward and also the check method from express validator
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('name', 'Name is required')
+				.not()
+				.isEmpty()
+		]
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, phone, type } = req.body;
+
+		try {
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id
+			});
+
+			const contact = await newContact.save();
+
+			res.json(contact);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
