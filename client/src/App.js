5824e08145abf2ec19d1ace1832a2906416cc25d
Pat Palmerston <patpalmerston@gmail.com>
2019-07-28 22:53:01 -0700

    basic set up navbar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 import './App.css';
 
-function App() {
-	return <div className='App' />;
-}
+const App = () => {
+	return (
+		<Router>
+			<Fragment>
+				<div className='App'>
+					<Navbar />
+					<div className='container'>
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/about' component={About} />
+						</Switch>
+					</div>
+				</div>
+			</Fragment>
+		</Router>
+	);
+};
 
 export default App;
