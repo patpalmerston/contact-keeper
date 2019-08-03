206c7e0448d09dd217e1d78daff97dc0ab6f7e73
Pat Palmerston <patpalmerston@gmail.com>
2019-08-03 11:14:07 -0700

    update contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -86,6 +86,32 @@ const ContactState = props => {
 			});
 		}
 	};
+	// Update Contact
+	const updateContact = async contact => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		try {
+			const res = await axios.put(
+				`/api/contacts/${contact._id}`,
+				contact,
+				config
+			);
+
+			dispatch({
+				type: UPDATE_CONTACT,
+				payload: res.data
+			});
+		} catch (err) {
+			dispatch({
+				type: CONTACT_ERROR,
+				payload: err.response.msg
+			});
+		}
+	};
 
 	// Clear Contacts
 	const clearContacts = () => {
@@ -100,11 +126,6 @@ const ContactState = props => {
 	// Clear Current Contact
 	const clearCurrent = () => {
 		dispatch({ type: CLEAR_CURRENT });
-	};
-
-	// Update Contact
-	const updateContact = contact => {
-		dispatch({ type: UPDATE_CONTACT, payload: contact });
 	};
 
 	// Filter Contacts
