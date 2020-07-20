eceadadba25da2318c280c6b7f9c7bb7e919bf85
Pat Palmerston <patpalmerston@gmail.com>
2020-07-20 13:50:33 -0700

    prep for deploy

--- server.js
+++ server.js
@@ -16,13 +16,15 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
+// check if in production
 if (process.env.NODE_ENV === 'production') {
-	// set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	);
+    // set static folder
+    // if yes then load a static folder
+    app.use(express.static('client/build'));
+    // use this route to load the index html if it hit
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
 
 const PORT = process.env.PORT || 5000;
